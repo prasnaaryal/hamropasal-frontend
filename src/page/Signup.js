e822d6d800681212ac3fabcd905ff524208601b5
prasnaaryal <prasna101@gmail.com>
2023-12-20 21:28:08 +0545

    hotfix

--- src/page/Signup.js
+++ src/page/Signup.js
@@ -4,11 +4,10 @@ import loginimage from "../assets/loginimage.gif";
 import { Link, useNavigate } from "react-router-dom";
 import { ImagetoBase64 } from "../utility/ImagetoBase64";
 
-const Signup = () => {
+function Signup() {
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
-  const [confirmPassword, setShowConfirmpassword] = useState(false);
-  //to store value
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [data, setData] = useState({
     firstName: "",
     lastName: "",
@@ -23,12 +22,11 @@ const Signup = () => {
   const handleShowPassword = () => {
     setShowPassword((preve) => !preve);
   };
-
   const handleShowConfirmPassword = () => {
-    setShowConfirmpassword((preve) => !preve);
+    setShowConfirmPassword((preve) => !preve);
   };
 
-  const handleOnchange = (e) => {
+  const handleOnChange = (e) => {
     const { name, value } = e.target;
     setData((preve) => {
       return {
@@ -54,29 +52,24 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { firstName, email, password, confirmPassword } = data;
-
     if (firstName && email && password && confirmPassword) {
       if (password === confirmPassword) {
-        try {
-          const fetchData = await fetch(
-            `${process.env.REACT_APP_SERVER_DOMAIN}/signup`,
-            {
-              method: "POST",
-              headers: {
-                "content-type": "application/json",
-              },
-              body: JSON.stringify(data),
-            }
-          );
-
-          const dataRes = await fetchData.json();
-
-          if (dataRes.alert) {
-            navigate("/login");
+        const fetchData = await fetch(
+          `${process.env.REACT_APP_SERVER_DOMAIN}/signup`,
+          {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(data),
           }
-        } catch (error) {
-          console.error("Failed to fetch:", error);
-          // Handle the error as needed
+        );
+
+        const dataRes = await fetchData.json();
+
+        alert(dataRes.message);
+        if (dataRes.alert) {
+          navigate("/login");
         }
       } else {
         alert("password and confirm password not equal");
@@ -87,9 +80,10 @@ const Signup = () => {
   };
 
   return (
-    <div className="p-3 md:pd-4 ">
+    <div className="p-3 md:p-4">
       <div className="w-full max-w-sm bg-white m-auto flex  flex-col p-4">
-        <div className="w-20 h-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto ">
+        {/* <h1 className='text-center text-2xl font-bold'>Sign up</h1> */}
+        <div className="w-20 h-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto relative ">
           <img
             src={data.image ? data.image : loginimage}
             className="w-full h-full"
@@ -117,8 +111,8 @@ const Signup = () => {
             id="firstName"
             name="firstName"
             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            value={data.firstName} //name and value name must be similar
-            onChange={handleOnchange}
+            value={data.firstName}
+            onChange={handleOnChange}
           />
 
           <label htmlFor="lastName">Last Name</label>
@@ -128,7 +122,7 @@ const Signup = () => {
             name="lastName"
             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
             value={data.lastName}
-            onChange={handleOnchange}
+            onChange={handleOnChange}
           />
 
           <label htmlFor="email">Email</label>
@@ -138,18 +132,18 @@ const Signup = () => {
             name="email"
             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
             value={data.email}
-            onChange={handleOnchange}
+            onChange={handleOnChange}
           />
 
           <label htmlFor="password">Password</label>
-          <div className="flex px-2 py-1 bg-slate-200 px-2 py-1 rounded mt-1 mb-2  focus-within:outline-blue-300">
+          <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
             <input
               type={showPassword ? "text" : "password"}
               id="password"
               name="password"
-              className="w-full bg-slate-200 border-none outline-none"
+              className=" w-full bg-slate-200 border-none outline-none "
               value={data.password}
-              onChange={handleOnchange}
+              onChange={handleOnChange}
             />
             <span
               className="flex text-xl cursor-pointer"
@@ -159,31 +153,30 @@ const Signup = () => {
             </span>
           </div>
 
-          <label htmlFor="confirmpassword"> Confirm Password</label>
-          <div className="flex px-2 py-1 bg-slate-200 px-2 py-1 rounded mt-1 mb-2  focus-within:outline-blue-300">
+          <label htmlFor="confirmpassword">Confirm Password</label>
+          <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2  focus-within:outline focus-within:outline-blue-300">
             <input
-              type={confirmPassword ? "text" : "password"}
+              type={showConfirmPassword ? "text" : "password"}
               id="confirmpassword"
               name="confirmPassword"
-              className="w-full bg-slate-200 border-none outline-none"
+              className=" w-full bg-slate-200 border-none outline-none "
               value={data.confirmPassword}
-              onChange={handleOnchange}
+              onChange={handleOnChange}
             />
             <span
               className="flex text-xl cursor-pointer"
               onClick={handleShowConfirmPassword}
             >
-              {confirmPassword ? <BiShow /> : <BiHide />}
+              {showConfirmPassword ? <BiShow /> : <BiHide />}
             </span>
           </div>
 
-          <button className="w-full max-w-[150px] m-auto bg-red-500 hover:bg-red-600 cursor-pointer  text-white text-xl font-medium text-center py-1 rounded-full mt-4">
+          <button className="w-full max-w-[150px] m-auto  bg-red-500 hover:bg-red-600 cursor-pointer  text-white text-xl font-medium text-center py-1 rounded-full mt-4">
             Sign up
           </button>
         </form>
-
-        <p className="text-left text-sm my-2">
-          Already have an account?
+        <p className="text-left text-sm mt-2">
+          Already have account ?{" "}
           <Link to={"/login"} className="text-red-500 underline">
             Login
           </Link>
@@ -191,6 +184,6 @@ const Signup = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Signup;
