9329dff21be8835ac5cbabacc269757730848391
prasnaaryal <prasna101@gmail.com>
2024-01-14 14:48:00 +0545

    updated login design

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BiHide, BiShow } from "react-icons/bi";
 import loginimage from "../assets/loginimage.gif";
+import authimg from "../assets/authimg.png";
 import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
@@ -71,40 +72,49 @@ const Login = () => {
 
   console.log(userData);
   return (
-    <div className="p-3 md:pd-4 ">
-      <div className="w-full max-w-sm bg-white m-auto flex  flex-col p-4">
+    <div className=" bg-white grid grid-cols-12 gap-6 py-10 px-3 md:px-4 ">
+      {/* image */}
+      <div className="col-span-6 w-full flex justify-center items-center">
+        <img src={authimg} className="w-[600px]" alt="authimg" />
+      </div>
+
+      <div className="w-full max-w-sm  m-auto flex border rounded-lg flex-col col-span-6 p-6">
         <div className="w-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto">
           <img src={loginimage} className="w-full" />
         </div>
 
         {/* //text form  */}
-        <form className="w-full py-3 flex flex-col" onSubmit={handleSubmit}>
-          <label htmlFor="email">Email</label>
-          <input
-            type={"email"}
-            id="email"
-            name="email"
-            className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            value={data.email}
-            onChange={handleOnchange}
-          />
 
-          <label htmlFor="password">Password</label>
-          <div className="flex px-2 py-1 bg-slate-200 px-2 py-1 rounded mt-1 mb-2  focus-within:outline-blue-300">
+        <form className="w-full py-3 flex flex-col" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
             <input
-              type={showPassword ? "text" : "password"}
-              id="password"
-              name="password"
-              className="w-full bg-slate-200 border-none outline-none"
-              value={data.password}
+              type={"email"}
+              id="email"
+              name="email"
+              className="mt-1 mb-2 w-full px-2 py-2 rounded focus-within:outline-blue-300 border"
+              value={data.email}
               onChange={handleOnchange}
             />
-            <span
-              className="flex text-xl cursor-pointer"
-              onClick={handleShowPassword}
-            >
-              {showPassword ? <BiShow /> : <BiHide />}
-            </span>
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <div className="flex px-2 py-2 rounded mt-1 mb-2  border focus-within:outline-blue-300">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                name="password"
+                className="w-full outline-none"
+                value={data.password}
+                onChange={handleOnchange}
+              />
+              <span
+                className="flex text-xl cursor-pointer"
+                onClick={handleShowPassword}
+              >
+                {showPassword ? <BiShow /> : <BiHide />}
+              </span>
+            </div>
           </div>
 
           <button className="w-full max-w-[150px] m-auto bg-red-500 hover:bg-red-600 cursor-pointer  text-white text-xl font-medium text-center py-1 rounded-full mt-4">
@@ -112,12 +122,14 @@ const Login = () => {
           </button>
         </form>
 
-        <p className="text-left text-sm my-2">
-          Dont have an account?{" "}
-          <Link to={"/signup"} className="text-red-500 underline">
-            Sign Up
-          </Link>
-        </p>
+        <div className="flex justify-center">
+          <p className="text-left text-sm my-2">
+            Dont have an account?{" "}
+            <Link to={"/signup"} className="text-red-500 underline">
+              Sign Up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
